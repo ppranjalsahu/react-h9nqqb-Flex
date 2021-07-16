e2e3f3b98d654aa7b50656cd28565e2784678750
ppranjalsahu <psahu@ce.iitr.ac.in>
2021-07-16 12:41:42 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,105 +1,45 @@
-import React from "react";
-import "./style.css";
-
-export default class Dashboard extends React.Component{
-  render(){
-    return(
-      <>
-      <div>
-      <div style={{
-        display: 'flex',
-        flexDirection: 'row',
-        padding: 10,
-        border: '1px solid black',
-        alignItems: 'stretch',
-        justifyContent: 'flex-start',
-        flexWrap: 'wrap',
-        backgroundColor:'grey',
-        borderRadius:'7px'
-      }}>
-        <div style={{
-          display: 'flex',
-          display: 'inline-block',
-          width: '100px',
-          height: '35px',
-          background: 'black',
-          borderRadius:'10px'
-          
-        }}>
-        <div className="content">GitaQuest</div>   
-        </div>
-        <div className="pic">
-
-        </div>
-        
-
-      </div>
-      <div className="blocks">
-    <div className="c2">
-      <div className="boxes">
-        
-        <div className="icon">
-            
-        </div>
-        <div className="content">Hello</div>
-          </div>
-
-          <div className="boxes">
-            <div className="icon">
-
-            </div>
-            <div className="content">Hello</div>
-          </div>
-          <div className="boxes">
-            <div className="icon">
-
-            </div>
-            <div className="content">Hello</div>
-          </div>
-          <div className="boxes">
-            <div className="icon">
-
-            </div>
-            <div className="content">Hello</div>
-          </div>
-          <div className="boxes">
-            <div className="icon">
-
-            </div>
-            <div className="content">Hello</div>
-          </div>
-        
-      </div>
-
-        <div className="window1">
-          <div className="window">
-           
-          <div className="white-board">
-
-          </div>
-          </div>
-          <div className="bottom">
-
-            <div className="bottom-two"></div>
-            <div className="bottom-two"></div>
-
-
-          </div>
-        </div>
-
-
-
-      </div>
-      </div>
-      </>
-    )
-  }
-}
+import React from 'react';
+import './style.css';
 
 export default function App() {
   return (
-    <div>
-      <Dashboard />
+    <div style={{ height: '100vh', overflow: 'hiddden' }}>
+      <div
+        style={{
+          width: '100%',
+          height: '50px',
+          backgroundColor: '#1a1a1a',
+          display: 'flex',
+          flexDirection : 'row',
+          padding: 10
+        }}
+      >
+        <div style={{ height: 30, width: 80, backgroundColor: '#fff'}}>
+          <div>
+            GitaQuest
+          </div>
+        </div>
+        <div
+          style={{
+            height: 30,
+            width: 30,
+            borderRadius: 1000,
+            backgroundColor: '#fff',
+            marginLeft: 'auto',
+          }}
+        />
+      </div>
+
+      <div style={{ display: 'flex', height: '100%' }}>
+        <div style={{ width: 150, height: '100%', backgroundColor: '#999' }} />
+        <div
+          style={{
+            height: '100%',
+            backgroundColor: '#ccc',
+            flexGrow: 1
+          }}
+        />
+      </div>
     </div>
   );
 }
