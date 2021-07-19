c8fbef6465bc83690bc351ad596fdb72aa1defd3
ppranjalsahu <psahu@ce.iitr.ac.in>
2021-07-19 22:14:38 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -8,34 +8,52 @@ export default function App() {
         style={{
           width: '100%',
           height: '50px',
-          backgroundColor: '#1a1a1a',
+          backgroundColor: '#420061',
           display: 'flex',
-          flexDirection : 'row',
-          padding: 10
+          flexDirection: 'row',
+          padding: 10,
+          position: 'fixed'
         }}
       >
-        <div style={{ height: 30, width: 80, backgroundColor: '#fff'}}>
-          <div>
-            GitaQuest
-          </div>
-        </div>
+        <img src = "C:\Users\Pranjal\Downloads\logo.svg" />
+        <div />
         <div
           style={{
             height: 30,
             width: 30,
             borderRadius: 1000,
-            backgroundColor: '#fff',
-            marginLeft: 'auto',
+            backgroundColor: '#ff9e90',
+            marginLeft: 'auto'
           }}
         />
       </div>
 
       <div style={{ display: 'flex', height: '100%' }}>
-        <div style={{ width: 150, height: '100%', backgroundColor: '#999' }} />
+        <div
+          style={{
+            width: 150,
+            height: '100%',
+            backgroundColor: '#ff8f0f',
+            padding: 10
+          }}
+        >
+          {[0, 1, 2, 3, 4].map(value => (
+            <button
+              style={{
+                width: '100%',
+                height: '30px',
+                backgroundColor: '#ffc380',
+                marginBottom: '10px',
+                marginTop: '10px',
+                borderRadius: '5px'
+              }}
+            />
+          ))}
+        </div>
         <div
           style={{
             height: '100%',
-            backgroundColor: '#ccc',
+            backgroundColor: '',
             flexGrow: 1
           }}
         />
